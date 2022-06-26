5e7721b9198da42dd118e901bd7b7b914fc41cfa
Ak4zh <agwl.akash@gmail.com>
2022-06-26 10:09:06 +0530

    initial commit

---
+++ src/lib/siteConfig.js
@@ -0,0 +1,14 @@
+export const SITE_URL = 'https://ak4zh.com';
+export const APPROVED_POSTERS_GH_USERNAME = ['ak4zh'];
+export const GH_USER_REPO = 'ak4zh/ak4zh'; // used for pulling github issues and offering comments
+export const REPO_URL = 'https://github.com/' + GH_USER_REPO;
+export const SITE_TITLE = '@ak4zh';
+export const SITE_DESCRIPTION = "ak4zh's blog";
+export const DEFAULT_OG_IMAGE =
+	'https://user-images.githubusercontent.com/6764957/147861359-3ad9438f-41d1-47c8-aa05-95c7d18497f0.png';
+export const MY_TWITTER_HANDLE = 'iak4zh';
+export const MY_YOUTUBE = 'https://www.youtube.com/c/ak4zh';
+
+// dont forget process.env.GH_TOKEN
+// if supplied, raises rate limit from 60 to 5000
+// https://docs.github.com/en/rest/overview/resources-in-the-rest-api#rate-limiting
